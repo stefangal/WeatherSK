764940f2629445498801f223227ba43676b54076
Stefan Gal <stefan.mail.sk@gmail.com>
2019-10-15 08:44:31 +0200

    Cleaned, only data collection to json format

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import csv
 import datetime
 import pandas as pd
+from pprint import pprint
 
 call = "https://api.openweathermap.org/data/2.5/weather?q="
 api_key = "4068916e2cc93f8de785229efdc8a78f"
@@ -13,45 +14,23 @@ def GetCitiesData(city_list):
     Give list of cities to get a Json file with data. 1 data report withe date & time key.
     """
     city_weather_data = dict()
+    clean_list = []
+    city = 'Hurbanovo'
 
-    #tim = str(datetime.datetime.today())[:16]
+    r = requests.get(url=call+city+'&units=metric' +"&appid="+ api_key)
 
-    for city in city_list:
-        #load city by city
-        r = requests.get(url=call+city+'&units=metric' +"&appid="+ api_key)
+    if r.status_code == 200:
+        data =json.loads(r.content)
+        pprint(data)
+    else:
+        print(f'Cannot access OpenWeatherMap.org for data. Error code: {r.statuscode}')
 
-        if r.status_code == 200:
-            city_weather_data =json.loads(r.content)
-            #print(city_weather_data)
-            train = pd.DataFrame.from_dict(city_weather_data, orient='index')
-            print(train)
-            # city_weather_data[city] = list([k, v] for k, v in json_data['main'].items())
-            # print([a, b] for a, b in dict(v for v in json_data['weather']).items())
-            # #city_weather_data[city].append([[a, b] for a, b in dict(v for v in json_data['weather']).items()])
-            # #city_weather_data[city] = json_data['wind']
-        else:
-            print(f"ISSUE: {r.status_code} with city: {city}")
-
-    print(f"**** Status OK, code: {r.status_code} ****")
-    #print(city_weather_data)
-    print(datt)
     # Save to JSON file: weather.json
     with open(r'JSON\weathers.json' , 'w') as f:
-        json.dump(city_weather_data, f, indent=4)
-    return city_weather_data
+        json.dump(data, f, indent=4)
+    return data
 
-
-def CityData(file, city):
-    with open(file) as f:
-        data = json.load(f)
-        df = pd.DataFrame.from_dict(data, orient='index')
-        print(df)
-        # print(city)
-        # print("**********")
-        # for k,v in data["2019-10-14 14:10"][city].items():
-        #     print(k, v)
 
 city_db = ["Bratislava", "Kosice", "Hurbanovo", "Nitra"]
 
 GetCitiesData(city_db)
-CityData(file='JSON\weathers.json', city='Hurbanovo')
